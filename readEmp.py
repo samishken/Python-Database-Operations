50645723e4b5f6a67dc8c7b8bb9ac7345f412849
Samuel Haile <samishken@gmail.com>
2019-10-24 20:26:26 -0400

    update read mysql

--- readEmp.py
+++ readEmp.py
@@ -5,16 +5,16 @@ conn = mysql.connector.connect(host='localhost',database='samboo',user='root',pa
 if conn.is_connected():
     print("Connected to mysql db")
 
-cursor = con.cursor()
+cursor = conn.cursor()
 
 cursor.execute('select * from emp')
-print("Total Number of records",cursor.rowcont)
+print("Total Number of records",cursor.rowcount)
 
 #fetch one record at a time
 row = cursor.fetchone()
 while row is not None:
     print(row)
-    row=cursor.fechone() #fetch next record
+    row=cursor.fetchone() #fetch next record
 
 #fetch all record at once
 row = cursor.fetchall()
